7bfa6cf9c12e1bc2b6e71441cf6881aa2bfc0211
lucabritten <lucabritten17@icloud.com>
2025-03-08 20:33:39 +0100

    Added a Codeopolis class to start the game

---
+++ Codeopolis.java
@@ -0,0 +1,8 @@
+public class Codeopolis {
+
+    public static void main(String[] args){
+
+        TextInterface textInterface = new TextInterface();
+        textInterface.showMainMenu();
+    }
+}
